fc3b038bd7f672377111c73137c2f4b60850bb51
olivia <111635889+oliviaow2022@users.noreply.github.com>
2025-03-12 22:11:22 +0800

    feat: geminiProvider

--- projects/extension/src/llm/geminiProvider.ts
+++ projects/extension/src/llm/geminiProvider.ts
@@ -8,5 +8,20 @@ export class GeminiProvider implements LLMProvider {
       this.apiKey = apiKey;
     }
     async generateResponse(systemPrompt: string, userPrompt: string): Promise<string> {
+        try {
+            const gemini = new GoogleGenerativeAI(this.apiKey);
+            const model = gemini.getGenerativeModel({
+                model: "gemini-1.5-flash-latest",
+                generationConfig: {
+                    responseMimeType: "application/json"
+                }
+            })
+            const result = await model.generateContent(userPrompt)
+            console.log("gemini result:", result)
+            return result.response.text(); // Extract response text
+          } catch (error) {
+            console.error("Error calling Gemini API:", error);
+            throw new Error("Failed to generate response from Gemini.");
+          }
     } 
 }
