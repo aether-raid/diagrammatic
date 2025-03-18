0b89e0933185793a60b64b0d717954b7ede3441c
kahlia <alex.c0304@hotmail.com>
2025-03-18 21:02:32 +0800

    refactor: contexts to main.tsx, global message handler to App.tsx

--- projects/webview-ui/src/App.tsx
+++ projects/webview-ui/src/App.tsx
@@ -1,18 +1,74 @@
+import { useEffect } from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom"
 
+import {
+    AcceptComponentDiagramDataPayload,
+    AcceptNodeEdgeDataPayload,
+    Commands,
+    WebviewCommandMessage
+} from "@shared/message.types";
+
+import { useNodeEdgeDataContext } from "./contexts/NodeEdgeDataContext";
+import { sendReadyMessageToExtension } from "./helpers/vscodeApiHandler";
 import CodeView from "./views/CodeView"
 import ComponentView from "./views/ComponentView"
-import { NodeEdgeDataProvider } from "./contexts/NodeEdgeDataContext"
 
 export const App = () => {
-  return (
-    <NodeEdgeDataProvider>
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<CodeView />} />
-          <Route path="/componentView" element={<ComponentView />} />
-        </Routes>
-      </MemoryRouter>
-    </NodeEdgeDataProvider>
-  )
+    const nodeEdgeCtx = useNodeEdgeDataContext();
+
+    useEffect(() => {
+        // Setup message listener
+        const onMessage = (event: MessageEvent<WebviewCommandMessage>) => {
+            const { command, message } = event.data;
+
+            switch (command) {
+                case Commands.ACCEPT_NODE_EDGE_DATA: {
+                    const msg = message as AcceptNodeEdgeDataPayload;
+                    // msg.nodes = retainNodePositions(msg.nodes, nodesRef.current);
+                    nodeEdgeCtx?.setCodeNodeEdgeData({
+                        nodes: msg.nodes,
+                        edges: msg.edges,
+                    });
+                    break;
+                }
+                case Commands.ACCEPT_COMPONENT_DIAGRAM_DATA: {
+                    const msg = message as AcceptComponentDiagramDataPayload;
+                    // msg.nodes = retainNodePositions(msg.nodes, nodesRef.current);
+                    nodeEdgeCtx?.setComponentNodeEdgeData({
+                        nodes: msg.nodes,
+                        edges: msg.edges,
+                    });
+                    break;
+                }
+            }
+        };
+
+        window.addEventListener("message", onMessage);
+
+        try {
+            sendReadyMessageToExtension();
+        } catch (error) {
+            if (
+                (error as Error).message !==
+                "acquireVsCodeApi is not defined"
+            ) {
+                // Only catch the above error, throw all else
+                throw error;
+            }
+        }
+
+        return () => {
+            // Remove event listener on component unmount
+            window.removeEventListener("message", onMessage);
+        };
+    }, []);
+
+    return (
+        <MemoryRouter>
+            <Routes>
+                <Route path="/" element={<CodeView />} />
+                <Route path="/componentView" element={<ComponentView />} />
+            </Routes>
+        </MemoryRouter>
+    )
 }
