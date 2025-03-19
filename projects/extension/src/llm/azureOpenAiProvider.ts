deb5beb89b7f6a2fbb82395d4aa2c05a44607a4d
olivia <111635889+oliviaow2022@users.noreply.github.com>
2025-03-19 23:48:03 +0800

    fix: azure openai sdk key authentication format

--- projects/extension/src/llm/azureOpenAiProvider.ts
+++ projects/extension/src/llm/azureOpenAiProvider.ts
@@ -1,40 +1,45 @@
 import { LLMProvider } from "../helpers/llm";
-import axios from "axios";
+import { AzureOpenAI } from "openai";
 
-export class AzureOpenAIProvider implements LLMProvider{
+export class AzureOpenAIProvider implements LLMProvider {
     private readonly apiKey: string;
     private readonly endpoint: string;
     private readonly deploymentId: string;
+    private readonly modelName: string;
     private readonly apiVersion: string
 
-    constructor(apiKey: string, endpoint: string, deploymentId: string, apiVersion: string) {
+    constructor(apiKey: string, endpoint: string, modelName: string, deploymentId: string, apiVersion: string) {
         this.apiKey = apiKey;
         this.endpoint = endpoint;
         this.deploymentId = deploymentId;
+        this.modelName = modelName;
         this.apiVersion = apiVersion;
     }
 
     async generateResponse(systemPrompt: string, userPrompt: string): Promise<string> {
+        const endpoint = this.endpoint;
+        const apiKey = this.apiKey;
+        const apiVersion = this.apiVersion;
+        const deploymentId = this.deploymentId;
+        const modelName = this.modelName;
+        const options = { endpoint, apiKey, deploymentId, apiVersion }
+        const client = new AzureOpenAI(options);
         try {
-          const response = await axios.post(
-            `${this.endpoint}/openai/deployments/${this.deploymentId}/completions?api-version=${this.apiVersion}-preview`,
-            {
-              prompt: [userPrompt],
-              temperature: 0,
-            },
-            {
-              headers: {
-                "Content-Type": "application/json",
-                "api-key": this.apiKey,
-              },
-            }
-          );
-    
-          return response.data.choices[0].message.content;
+            const response = await client.chat.completions.create({
+                messages: [
+                    { role: "system", content: systemPrompt },
+                    { role: "user", content: userPrompt }
+                ],
+                temperature: 0,
+                model: modelName
+            });
+
+            return response.choices[0].message.content ?? "";
+
         } catch (error) {
             console.error("Error calling Gemini API:", error);
             throw new Error("Failed to generate response from Azure OpenAI.");
-          }
+        }
 
     }
 }
