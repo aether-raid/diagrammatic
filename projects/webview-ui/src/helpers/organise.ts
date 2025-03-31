2711a49ba8a4d14e28bfec537fb737e0f67cb1db
BruceWu2001 <brucewuzijian@gmail.com>
2025-03-31 12:20:26 +0800

    feat: add ruleId to diagnostic

--- projects/webview-ui/src/helpers/organise.ts
+++ projects/webview-ui/src/helpers/organise.ts
@@ -1,19 +1,19 @@
 import { SerializedDiagnostic } from "@shared/vscode.types";
 
 type GroupedIssues = {[key:string]: SerializedDiagnostic[]};
-type ClassifiedIssues = {[key:string]: {
-    message: string;
-    severity: number;
-    lineNumbers: number[];
-}};
+// type ClassifiedIssues = {[key:string]: {
+//     message: string;
+//     severity: number;
+//     lineNumbers: number[];
+// }};
 export const groupIssues = (issues: SerializedDiagnostic[]): GroupedIssues => {
     const groupedIssues = issues.reduce<GroupedIssues>((acc, issue) => {
-        const {source} = issue
-        if (!source){
+        const {rule} = issue
+        if (!rule){
             return acc;
         }
-        acc[source] = acc[source] || [];
-        acc[source].push(issue);
+        acc[rule] = acc[rule] || [];
+        acc[rule].push(issue);
         return acc;
     }, {});
     return groupedIssues;
