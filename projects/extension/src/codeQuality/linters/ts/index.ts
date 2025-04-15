c5b1e60db72680ca59df320cbbeba544c3adca32
BruceWu2001 <zijian.wu.2022@smu.edu.sg>
2025-04-15 17:52:33 +0800

    fix: path to lint config ~ mac fix

--- projects/extension/src/codeQuality/linters/ts/index.ts
+++ projects/extension/src/codeQuality/linters/ts/index.ts
@@ -1,8 +1,9 @@
 import { ESLint } from 'eslint';
 import * as vscode from "vscode";
+import * as path from "path";
 
 const extension = vscode.extensions.getExtension('diagrammatic.diagrammatic')!;
-const configFilePath = `${extension.extensionPath}\\config\\linting-configs\\eslint.config.mjs`
+const configFilePath = path.join(extension.extensionPath, 'config', 'linting-configs', 'eslint.config.mjs');
 
 
 export const lintFileES = (filePath:string, cwd:string): Promise<ESLint.LintResult[]> | [] => {
